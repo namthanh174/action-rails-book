0c5913682967474548a2bf3941c0ce146f6e4a46
thanh vo <namthanh174@gmail.com>
2017-12-08 03:53:01 +0000

    Implemented sign up.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/show'
+
+  get 'users/create'
+
+  get 'users/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -58,4 +68,6 @@ Rails.application.routes.draw do
   resources :projects do
     resources :tickets
   end
+  
+  resources :users
 end
