70c119764c7fe6dfc14f3a8a5fd6f59c5a7d565e
thanh vo <namthanh174@gmail.com>
2017-12-15 06:57:59 +0000

    User can upload 3 files at a time

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -9,6 +9,7 @@ class TicketsController < ApplicationController
     
     def new
        @ticket = @project.tickets.build 
+       3.times{ @ticket.assets.build }
     end
     
     def create
@@ -49,7 +50,7 @@ class TicketsController < ApplicationController
     
     private 
         def ticket_params 
-            params.require(:ticket).permit(:title, :description, :asset)
+            params.require(:ticket).permit(:title, :description, assets_attributes: [:asset])
         end
         
         def set_project
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,5 +4,6 @@ class Ticket < ActiveRecord::Base
   validates :title, presence: true
   validates :description, presence: true, length: { minimum: 10}
   
-  mount_uploader :asset, AssetUploader
+  has_many :assets
+  accepts_nested_attributes_for :assets
 end
